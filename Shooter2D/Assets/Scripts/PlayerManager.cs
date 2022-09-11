f606fc58d419d82a035c3ab8a966f5f64fcb137f
denizvarici <denizvarici@hotmail.com>
2022-09-11 23:46:40 +0300

    improvements
    simple game scene design

---
+++ Shooter2D/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    //https:.//www.youtube.com/watch?v=wZUPCSuqklM
+    //https:.//www.youtube.com/watch?v=UsGuN69g2NI&t=184s
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
